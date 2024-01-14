4606a4b212c6ae4abd12fe3061180934b91fec56
abhijith8176 <abhijith8176@gmail.com>
2024-01-14 22:32:00 +0530

    test

--- database/db.py
+++ database/db.py
@@ -1,10 +1,8 @@
 def fibonacci_sequence(n):
     fib_sequence = [0, 1]
-
     while len(fib_sequence) < n:
         next_num = fib_sequence[-1] + fib_sequence[-2]
         fib_sequence.append(next_num)
-
     return fib_sequence
 
 # Prompt the user for the number of terms in the Fibonacci sequence is sequence
